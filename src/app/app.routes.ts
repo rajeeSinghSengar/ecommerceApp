68565c2ab585129c87b86e066fa82a5e50e10e2c
rajeeSinghSengar <rajeesinghsengar1997@gmail.com>
2025-01-19 18:40:44 +0530

    added routes to home and seller component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes, provideRouter } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect root to Home
+    { path: 'seller', component: SellerAuthComponent },
+    {path:'home',component:HomeComponent}
+  ];
+  
+
---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  imports: [],
+  templateUrl: './seller-auth.component.html',
+  styleUrl: './seller-auth.component.scss'
+})
+export class SellerAuthComponent {
+
+}
