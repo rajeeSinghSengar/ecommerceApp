c6ab95e8b7d1289a4264b2954515289dfef5105c
rajeeSinghSengar <rajeesinghsengar1997@gmail.com>
2025-01-25 18:36:03 +0530

    modfied navbar based on seller or default menutype

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Route, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+menuType : string = 'default';
+/**
+ *
+ */
+constructor(private route : Router) {
+  
+}
+ngOnInit(){
+  this.route.events.subscribe((val:any)=>{
+    //check if route url has seller
+    if(val.url && val.url.includes('seller')){
+      //u need to check if user is logged in 
+      if(localStorage.getItem('seller')){
+      this.menuType = 'seller';
+      }
+    }
+    else{
+      this.menuType = 'default';
+    }
+  })
+}
 }
