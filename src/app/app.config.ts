53edde48459d8e5f317248290ee39933da1ca07f
rajeeSinghSengar <rajeesinghsengar1997@gmail.com>
2025-02-08 13:15:42 +0530

    added angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
      provideRouter(routes), // This sets up Angular's Router for handling navigation and routing in the app.
-     provideHttpClient()//Registers Angular's HttpClient service to enable making HTTP requests to APIs
+     provideHttpClient(), //Registers Angular's HttpClient service to enable making HTTP requests to APIs
+     provideAnimationsAsync()
      ]
 };
