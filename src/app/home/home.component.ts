553c236d0218d89c726f85121f1df6012deaae04
rajeeSinghSengar <rajeesinghsengar1997@gmail.com>
2025-02-08 21:20:41 +0530

    static product list  implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgbModule,CommonModule,NgbCarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
 }
